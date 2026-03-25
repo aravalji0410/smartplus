88f623364b8d130b22afc7c83aa25c891a43df2d
Aayushi <aravalji2001@gmail.com>
2026-03-25 18:15:39 -0400

    changes in server js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const sequelize = require("./config/db");
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -21,20 +19,8 @@ app.get("/", (req, res) => {
   res.send("Smart+ API running");
 });
 
-// Port (Render compatible)
 const PORT = process.env.PORT || 5000;
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// DB connection
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("DB connected");
-  } catch (err) {
-    console.error("DB connection failed:", err);
-  }
-})();
