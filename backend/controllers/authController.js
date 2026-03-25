d2ac81a214132fba2ba9ccc3f998f49345daf0f1
Aayushi <aravalji2001@gmail.com>
2026-03-25 16:34:52 -0400

    changes in authcontroller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,12 +2,11 @@ const db = require("../config/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-// LOGIN USER
+// LOGIN FUNCTION
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Check if user exists
     const [rows] = await db.query(
       "SELECT * FROM Users WHERE email = ?",
       [email]
@@ -19,20 +18,17 @@ exports.login = async (req, res) => {
 
     const user = rows[0];
 
-    // Compare password
     const match = await bcrypt.compare(password, user.password);
 
     if (!match) {
       return res.status(400).json({ msg: "Wrong password" });
     }
 
-    // Generate token
     const token = jwt.sign({ id: user.userId }, "secretkey");
 
     res.json({ token });
 
   } catch (err) {
-    console.error(err);
     res.status(500).json({ msg: "Server error" });
   }
 };
